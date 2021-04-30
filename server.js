6cb25b7a5b6eb43d3bd0840ed8e549b4e8c64e5f
Tyler Johnson <tyler@csdconsultants.us>
2021-04-30 00:55:47 -0700

    refactoring to follow trello task details

--- server.js
+++ server.js
@@ -6,13 +6,28 @@ mongoose.connect('mongodb://localhost:27017/booksdb', {useNewUrlParser: true, us
 
 //intentionally added AFTER mongoose.connect
 const Book = require('./models/books.js');
+const User = require('./models/users.js');
+
+const myBook = new Book ({title: 'The First Book', author: 'Unknown', genre: 'Non-fiction'});
+myBook.save(function (err) {
+  if (err) console.err(err);
+  else console.log('saved the book');
+});
 
 app.get('/', (req, res) => {
   res.send('hello!');
 });
 
 app.get('/books', (req, res) => {
-  res.send('hello!');
+  Book.find((err, databaseResults) => {
+    res.send(databaseResults);
+  });
+});
+
+app.get('/users', (req, res) => {
+  Book.find((err, databaseResults) => {
+    res.send(databaseResults);
+  });
 });
 
 app.listen(3001, () => console.log('app listening on 3001'));
